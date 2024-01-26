3f5b355b0f731622a77dc8abad4fb72ec0d1413b
Chris Nesbitt-Smith <chris@cns.me.uk>
2024-01-26 15:21:40 +0000

    add cddo theme (#239)

--- themes/postcss.config.cjs
+++ themes/postcss.config.cjs
@@ -1,9 +1,11 @@
 module.exports = {
   plugins: [
+    require("postcss-discard-comments"),
     require("postcss-use"),
     require("postcss-nested"),
     require("postcss-for"),
+    require("postcss-math"),
+    require("postcss-simple-vars"),
     require("postcss-import"),
-    require("postcss-discard-comments"),
   ],
 };
