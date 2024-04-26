9541be9455f72547d6b9bc702e555b70712c69f3
Chris Nesbitt-Smith <chris@cns.me.uk>
2024-04-26 15:44:15 +0100

    script to generate stats

---
+++ stats.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+npm run build:txt  > /dev/null 2>&1
+
+words_per_minute=155
+
+table="Filename,Slides,Words,Minutes"
+
+for filename in dist/*.txt; do
+
+  slidecount=$(cat ${filename} | grep -- "---" | wc -w |tr -d '[:blank:]')
+  ((slidecount++))
+  if [ "$slidecount" -eq 1 ]; then
+    continue
+  fi
+  wordcount=$(cat ${filename} | grep -v -- "---" | wc -w| tr -d '[:blank:]')
+  minutes=$(echo "scale=2; $wordcount / $words_per_minute" | bc)
+
+  filename=${filename#dist/}
+  filename=${filename%.txt}
+  table+=" ${filename},${slidecount},${wordcount},${minutes}"
+done
+
+
+printf %s\\n ${table} |column -s, -t
+
+echo "\n\n\n\n\n\n\n\n\n\n\n\nExpecting you to run this with watch ./stats.sh"
